f44e506c4b3d892757b0fc0a9432724b3dcc98c1
AWilliamson88 <87967861+AWilliamson88@users.noreply.github.com>
2023-01-01 10:47:28 +0800

    Add IsNull, including tests

---
+++ Validations/Validations/IsNullValidationExtentions.cs
@@ -0,0 +1,21 @@
+﻿using PopValidations.Execution.Validations.Base;
+using PopValidations.FieldDescriptors.Base;
+using PopValidations.Validations;
+using System;
+
+namespace PopValidations
+{
+    public static class IsNullValidationExtentions
+    {
+        public static IFieldDescriptor<TValidationType, TFieldType> IsNull<TValidationType, TFieldType>(
+        this IFieldDescriptor<TValidationType, TFieldType> fieldDescriptor,
+        Action<ValidationOptions>? optionsAction = null
+        )
+        {
+            var validation = new IsNullValidation();
+            optionsAction?.Invoke(new ValidationOptions(validation));
+            fieldDescriptor.AddValidation(validation);
+            return fieldDescriptor;
+        }
+    }
+}
