388b3e1435261cbe0fa3c3c3f98766808d0ad980
AWilliamson88 <87967861+AWilliamson88@users.noreply.github.com>
2023-01-22 15:33:37 +0800

    Update IsEmptyValidationTests.cs

--- Validation_Tests/ValidationsTests/IsEmptyValidationTests/IsEmptyValidationTests.cs
+++ Validation_Tests/ValidationsTests/IsEmptyValidationTests/IsEmptyValidationTests.cs
@@ -1,6 +1,7 @@
 ﻿using FluentAssertions;
 using FluentAssertions.Execution;
 using PopValidations.Validations;
+using System.Collections;
 using System.Collections.Generic;
 using Xunit;
 
@@ -8,17 +9,65 @@ namespace PopValidations_Tests.ValidationsTests;
 
 public class IsEmptyValidationTests
 {
-    [Fact]
-    public void WhenSupplyingANullValue_ItValidatesAsSuccessful()
+    [Theory]
+    [MemberData(nameof(GetPassingData))]
+    public void WhenSupplyingANullOrEmptyValue_ItValidatesAsSuccessful(IEnumerable value)
     {
         // Arrange
         var validator = new IsEmptyValidation();
 
         // Act
-        var result = validator.Validate(null);
+        var result = validator.Validate(value);
 
         // Assert
         result.Success.Should().BeTrue();
+    }
+
+    public static IEnumerable<object[]> GetPassingData()
+    {
+        var allData = new List<object[]>
+        {
+            new object[] { System.Array.Empty<object>() },
+            new object[] { new List<object>() },
+            new object[] { new LinkedList<object>() },
+            new object[] { "" },
+            new object[] { " " },
+            new object[] { new Dictionary<object, object>() },
+            new object[] { null },
+        };
+
+        return allData;
+    }
+
+    [Theory]
+    [MemberData(nameof(GetFailingData))]
+    public void WhenSupplyingANonNullValue_ItValidatesAsUnsuccessful(IEnumerable value)
+    {
+        // Arrange
+        var validator = new IsEmptyValidation();
+
+        // Act
+        var result = validator.Validate(value);
+
+        // Assert
+        result.Success.Should().BeFalse();
+    }
+
+    public static IEnumerable<object[]> GetFailingData()
+    {
+        LinkedList<object> ll = new();
+        ll.AddFirst(new object());
+
+        var allData = new List<object[]>
+        {
+            new object[] { new int[] { 1, 2, 3 } },
+            new object[] { new List<object>() { new object() } },
+            new object[] { ll },
+            new object[] { "Hello" },
+            new object[] { new Dictionary<object, object>() { { new object(), new object() } } },
+        };
+
+        return allData;
     }
 
     [Fact]
